de581adb3149dfef9e9d06cb99cbb3de736d65ce
1650838218 <34147078+1650838218@users.noreply.github.com>
2018-11-06 16:44:43 +0800

    病例

---
+++ src/main/java/com/littledoctor/clinicassistant/module/cases/controller/CasesController.java
@@ -0,0 +1,18 @@
+package com.littledoctor.clinicassistant.module.cases.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: 周俊林
+ * @Date: 2018/11/6
+ * @Description: 病例
+ */
+@RestController
+@RequestMapping("/cases")
+public class CasesController {
+    /**
+     * 点击病例，右侧显示一个患者信息的列表，以患者姓名为查条件，有新建按钮
+     * 点击列表某行，进入病例页面，该页面分三部分，上部是患者的基本信息，左侧是单列的列表（列表内容是就诊日期），右侧
+     */
+}
